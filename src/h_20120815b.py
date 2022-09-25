b7069be2cdd398c4bdc0948f20d9c11ef56d1aec
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2022-09-25 22:19:37 +0000

    feat: Add h_20120815b.py

---
+++ src/h_20120815b.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+h_20120815b: Homework
+h_20120815b.py: Command line methods
+"""
+
+
+def cl_methods_b():
+    """
+    Command line methods B
+    """
+    n1 = int(input("Please Enter First Number: "))
+    n2 = int(input("Please Enter Second Number: "))
+    return n1 + n2
+
+
+def main():
+    """
+    Main function
+    """
+    print(cl_methods_b())
+
+
+if __name__ == "__main__":
+    main()
